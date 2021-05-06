8ee39a6ac77cc83d270d3a773ad7c7976b3b01fd
backbook <76886049@qq.com>
2021-05-06 16:58:42 +0800

    add wiki for spring boot

--- src/main/java/com/backbook/wiki/controller/TestController.java
+++ src/main/java/com/backbook/wiki/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.backbook.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +9,11 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @GetMapping("/")
+    public String wiki(){
+        return "welcome to wiki";
     }
 
 }
