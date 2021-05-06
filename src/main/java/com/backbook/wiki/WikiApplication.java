3e8f43ac0564c2d4e031c5aa4520a58194286e8b
backbook <76886049@qq.com>
2021-05-06 16:25:58 +0800

    add springboot base start

--- src/main/java/com/backbook/wiki/WikiApplication.java
+++ src/main/java/com/backbook/wiki/WikiApplication.java
@@ -1,13 +1,25 @@
 package com.backbook.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication application = new SpringApplication(WikiApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+
+
+        LOG.info("spring boot started");
+        LOG.info("url:\t http://127.0.0.0.1:{}",environment.getProperty("server.port"));
     }
+
 
 }
