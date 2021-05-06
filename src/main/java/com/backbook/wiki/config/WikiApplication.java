1aacb7bec90c4da88d0a16b560cfbfec4b8f05ef
backbook <76886049@qq.com>
2021-05-06 16:48:47 +0800

    add hello for spring boot

--- src/main/java/com/backbook/wiki/config/WikiApplication.java
+++ src/main/java/com/backbook/wiki/config/WikiApplication.java
@@ -1,13 +1,16 @@
-package com.backbook.wiki;
+package com.backbook.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+
 @SpringBootApplication
+//该类会扫描类下面的同级别及其子类
+@ComponentScan({"com.backbook.wiki"})
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
@@ -15,10 +18,9 @@ public class WikiApplication {
 
         SpringApplication application = new SpringApplication(WikiApplication.class);
         Environment environment = application.run(args).getEnvironment();
-
-
+        //添加注解用于,标识
         LOG.info("spring boot started");
-        LOG.info("url:\t http://127.0.0.0.1:{}",environment.getProperty("server.port"));
+        LOG.info("url:\t http://localhost:{}",environment.getProperty("server.port"));
     }
 
 
