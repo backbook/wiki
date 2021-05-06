0c88dc4f785bf6f0021a0f335a1ab479e2a5a85a
sujy <sujy@akulaku.com>
2021-05-06 23:03:22 +0800

    add Hot deployment

--- src/main/java/com/backbook/wiki/controller/TestController.java
+++ src/main/java/com/backbook/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.backbook.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testValue;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world " + testValue;
     }
 
     @PostMapping("/hello/post")
@@ -21,5 +25,6 @@ public class TestController {
     public String wiki(){
         return "welcome to wiki";
     }
+
 
 }
