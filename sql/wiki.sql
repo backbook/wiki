3d584aee1f7248b87cbcb5273a2da0c64dcd37b5
backbook <76886049@qq.com>
2021-05-08 21:20:01 +0800

    add ebook list restful api

--- sql/wiki.sql
+++ sql/wiki.sql
@@ -22,3 +22,16 @@ CREATE TABLE wiki.demo(
 
 INSERT INTO wiki.demo (id, name, password, create_time, update_time) VALUES (2, 'backbook', 'backbook', 1620376675418, 1620376675418);
 
+drop table if exists `ebook`;
+create table `ebook`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `doc_count` int comment '文档数',
+    `view_count`  int comment '阅读数',
+    `vote_count`  int comment '点赞数',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment ='电子书信息'
