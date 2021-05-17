9664d821dc2dbb76bf369d6063a5ce4e3592bfb9
backbook <76886049@qq.com>
2021-05-17 23:12:57 +0800

    home vue add list demo

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,24 +48,26 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter:20, column:3}" :data-source="ebooks">
+
+      <a-list item-layout="vertical" size="large" :grid="{ gutter:10, column:3}" :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
+            {{text}}
           </span>
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.name }}</a>
+                <a :href="item.name">{{ item.name }}</a>
               </template>
               <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
           </a-list-item>
         </template>
       </a-list>
+
     </a-layout-content>
   </a-layout>
 </template>
@@ -73,19 +75,6 @@
 <script lang="ts">
 import { defineComponent,onMounted,ref } from 'vue';
 import axios from 'axios'
-
-const listData: Record<string, string>[] = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
 
 
 export default defineComponent({
@@ -120,10 +109,20 @@ export default defineComponent({
     return{
       ebooks,
       // ebooks2: toRef(ebooks1, "books"),
-      listData,
       pagination,
       actions,
     }
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar{
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
+
