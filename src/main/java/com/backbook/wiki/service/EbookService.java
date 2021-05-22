7b64d60399144054ce425c2d62118a69664dcd7b
sujy <sujy@akulaku.com>
2021-05-22 23:42:00 +0800

    add pageHelper

--- src/main/java/com/backbook/wiki/service/EbookService.java
+++ src/main/java/com/backbook/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.backbook.wiki.domain.EbookExample;
 import com.backbook.wiki.mapper.EbookMapper;
 import com.backbook.wiki.req.EbookReq;
 import com.backbook.wiki.resp.EbookResp;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -17,6 +21,7 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
 
@@ -27,7 +32,13 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(ebookReq.getName())){
             criteria.andNameLike("%"+ebookReq.getName()+"%");
         }
+        //只针对于第一个sql有作用
+        PageHelper.startPage(1,3);
         List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebooks);
+        LOG.info("总行数: {}",pageInfo.getTotal());
+        LOG.info("总页数: {}",pageInfo.getPages());
+
         List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebooks) {
             EbookResp ebookResp = new EbookResp();
